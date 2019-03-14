628e675d8d5428c7ec35ba815c39ac8e6fbd25a2
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-14 21:58:48 +0100

    Added the possiblity to generate a code and let the PC guess (the pc will throw random numbers and will keep only the correct ones), you can select if you are the guesser by calling '#play(true)', if you want to create the code call '#play(false)'

--- mastermind.rb
+++ mastermind.rb
@@ -2,21 +2,70 @@ class Mastermind
     attr_accessor :code
     def initialize
         @responses = Hash.new
-        @responses[:correct] = assign_color
-        @responses[:color] = assign_color
-        @responses[:wrong] = assign_color
+        @responses[:correct] = -1
+        @responses[:color] = -2
+        @responses[:wrong] = -3
 
         @counter = 0
 
         @code = []
         4.times do
             @code << rand(4) + 1
-        end
-        introduction
+        end        
     end
 
-    def play
-        while @counter < 10 do
+    def play player
+        player ? play_player : play_PC
+    end
+
+    private
+
+    def play_PC
+        @ai_code = [0,0,0,0]
+        get_code
+        while @counter < 12
+            generate_code
+            puts "PC code: #{@ai_code}"
+            if check_for_win @ai_code
+                puts "PC wins"
+                return
+            end
+            return_tips @ai_code
+
+            @counter += 1
+        end
+        puts "PC lost"
+    end
+    def get_code
+        puts "Enter your code (Ex. 1 2 3 4)"
+        @code.clear
+        code = gets.chomp.split
+        code.each do |element|
+            @code << element.to_i
+        end
+    end
+
+    def generate_code
+        
+        code = []
+        @ai_code.each do |element|
+            code << element
+        end
+        @ai_code.clear
+
+        code.each_with_index do |element, index|
+            if element == @code[index]
+                @ai_code << element
+            else
+                
+                @ai_code << rand(4) + 1
+            end
+        end
+    end
+
+    def play_player
+        introduction
+        while @counter < 12 
             solution = []
             puts "Enter your numbers (Ex 1 3 2 4)"
             gets.chomp.split.each do |element|
@@ -24,22 +73,14 @@ class Mastermind
             end
             if check_for_win solution
                 puts "You win!"
-                break
+                return
             end
             return_tips solution
             @counter += 1
         end
+        puts "You lost!"
     end
-
-    private
-    def assign_color
-        loop do
-            value = rand(3) - 3
-            if !@responses.values.any?(value)
-                return value
-            end
-        end 
-    end
+    
     def check_for_win solution
         solution == @code ? true : false
     end
@@ -57,11 +98,10 @@ class Mastermind
     end
     def introduction
         puts "You will enter a value from 1 to 4 separeted by a blank space, whenever you do the game will tell you with a"
-        puts "random negative number (between -1 and -3) if you guessed the correct position and color, the correct color or if" 
-        puts "you guessed none"
+        puts "negative number if you guessed the correct position and color (-1), the correct color (-2) or if" 
+        puts "you guessed none (-3)"
     end
 end
 
 game = Mastermind.new
-puts game.code
-game.play
+game.play false
